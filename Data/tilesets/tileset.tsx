f7598d243e1a4d0636066f329a9ccc176d245578
blade-of-miquella <melinaprogg@gmail.com>
2024-04-22 22:22:10 +0300

    ver 0.2

---
+++ Data/tilesets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="96" tileheight="96" tilecount="207" columns="23">
+ <image source="../../Graphics/Drafts/tileset.png" width="2208" height="864"/>
+</tileset>
